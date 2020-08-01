633c65cd0cf0a4230abffd81290ca57810ba43c6
Greg Tarr <greg@tarr.com>
2020-08-01 13:32:14 +0100

    .

--- virbix/scripts/pool_check.sh
+++ virbix/scripts/pool_check.sh
@@ -9,7 +9,7 @@ CACHE_DIR="${APP_DIR}/${CACHE_DIR:-./var/cache}/pools"
 CACHE_FILE=${CACHE_DIR}/${UUID}.xml
 IMAGES_DIR=/var/lib/libvirt/images/
 
-rm -r ${CACHE_DIR}
+rm -rf ${CACHE_DIR}
 [ -d ${CACHE_DIR} ] || mkdir -p ${CACHE_DIR}
 ${VIRSH} pool-dumpxml ${UUID} > ${CACHE_FILE}
 chown -R zabbix.zabbix ${APP_DIR}
@@ -21,13 +21,13 @@ elif [[ ${ATTR} == 'size_free' ]]; then
 elif [[ ${ATTR} == 'size_total' ]]; then
     rval=`xmllint --xpath "string(//pool/capacity)" ${CACHE_FILE}`
 elif [[ ${ATTR} == 'size_cap_pc' ]]; then
-    size_cap=$(${VIRSH} vol-list --pool default --details | awk '{if ($5 == "GiB" ) {G+=$4;} else if($5 == "MiB") {G+=$4/1000} else if($5 == "TB") {G+=$4/1000}}  END {print G*1000000000}')
-    size_disk=$(df $(IMAGES_DIR) -B 1 |awk '{print $2}' |sed '2q;d')
+    size_cap=$(${VIRSH} vol-list --pool ${UUID} --details | awk '{if ($5 == "GiB" ) {G+=$4;} else if($5 == "MiB") {G+=$4/1000} else if($5 == "TB") {G+=$4/1000}}  END {print G*1000000000}')
+    size_disk=$(df ${IMAGES_DIR} -B 1 |awk '{print $2}' |sed '2q;d')
 
     rval=$(($(($size_cap*100))/$(($size_disk))))
 elif [[ ${ATTR} == 'size_alloc_pc' ]]; then
-    size_alloc=$(${VIRSH} vol-list --pool default --details | awk '{if ($7 == "GiB" ) {G+=$4;} else if($7 == "MiB") {G+=$6/1000} else if($7 == "TB") {G+=$6/1000}}  END {print G*1000000000}')
-    size_disk=$(df $(IMAGES_DIR) -B 1 |awk '{print $2}' |sed '2q;d')
+    size_alloc=$(${VIRSH} vol-list --pool ${UUID} --details | awk '{if ($7 == "GiB" ) {G+=$4;} else if($7 == "MiB") {G+=$6/1000} else if($7 == "TB") {G+=$6/1000}}  END {print G*1000000000}')
+    size_disk=$(df ${IMAGES_DIR} -B 1 |awk '{print $2}' |sed '2q;d')
 
     rval=$(($(($size_alloc*100))/$(($size_disk))))
 elif [[ ${ATTR} == 'state' ]]; then
